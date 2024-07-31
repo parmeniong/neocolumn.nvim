2409c5feba041743666c9624be8fef8a7d6b44ad
parmeniong <par.graikos@icloud.com>
2024-08-01 02:24:08 +0300

    Add automatic color blending for diagnostic near/far colors

--- lua/neocolumn/colors.lua
+++ lua/neocolumn/colors.lua
@@ -1,6 +1,68 @@
 local M = {}
 
+---@class RGB
+---@field r integer
+---@field g integer
+---@field b integer
+
+---@param hex string
+---@return RGB
+local function hex_to_rgb(hex)
+    hex = hex:gsub("#", "")
+    return {
+        r = tonumber("0x" .. hex:sub(1, 2)),
+        g = tonumber("0x" .. hex:sub(3, 4)),
+        b = tonumber("0x" .. hex:sub(5, 6))
+    }
+end
+
+---@param rgb RGB
+---@return string
+local function rgb_to_hex(rgb)
+    return string.format("#%02x%02x%02x", rgb.r, rgb.g, rgb.b)
+end
+
+---@param ... RGB
+---@return RGB
+local function mix_rgb(...)
+    local args = {...}
+
+    local out = { r = 0, g = 0, b = 0 }
+
+    for _, color in pairs(args) do
+        out = {
+            r = out.r + color.r / #args,
+            g = out.g + color.g / #args,
+            b = out.b + color.b / #args
+        }
+    end
+
+    out.r = math.floor(out.r + 0.5)
+    out.g = math.floor(out.g + 0.5)
+    out.b = math.floor(out.b + 0.5)
+
+    return out
+end
+
+---@param ... string
+---@return string
+local function mix_hex(...)
+    local args = vim.tbl_map(function(value)
+        return hex_to_rgb(value)
+    end, {...})
+    return rgb_to_hex(mix_rgb(unpack(args)))
+end
+
 function M.set_colors(colors, ns)
+    local error_near = mix_hex(colors.error, colors.normal, colors.error)
+    local error_far  = mix_hex(colors.error, colors.normal, colors.normal)
+    local warn_near  = mix_hex(colors.warn,  colors.normal, colors.warn)
+    local warn_far   = mix_hex(colors.warn,  colors.normal, colors.normal)
+    local info_near  = mix_hex(colors.info,  colors.normal, colors.info)
+    local info_far   = mix_hex(colors.info,  colors.normal, colors.normal)
+    local hint_near  = mix_hex(colors.hint,  colors.normal, colors.hint)
+    local hint_far   = mix_hex(colors.hint,  colors.normal, colors.normal)
+
     vim.api.nvim_set_hl(ns, "Neocolumn", {
         fg = colors.normal,
         bg = colors.bg
@@ -22,35 +84,35 @@ function M.set_colors(colors, ns)
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnErrorNear", {
-        fg = colors.error_near,
+        fg = error_near,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnErrorFar", {
-        fg = colors.error_far,
+        fg = error_far,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnWarnNear", {
-        fg = colors.warn_near,
+        fg = warn_near,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnWarnFar", {
-        fg = colors.warn_far,
+        fg = warn_far,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnInfoNear", {
-        fg = colors.info_near,
+        fg = info_near,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnInfoFar", {
-        fg = colors.info_far,
+        fg = info_far,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnHintNear", {
-        fg = colors.hint_near,
+        fg = hint_near,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnHintFar", {
-        fg = colors.hint_far,
+        fg = hint_far,
         bg = colors.bg
     })
     vim.api.nvim_set_hl(ns, "NeocolumnCursor", {
--- lua/neocolumn/config.lua
+++ lua/neocolumn/config.lua
@@ -13,17 +13,9 @@ M.defaults = {
     colors = {
         normal = "#7d7d7d",
         error = "#db4b4b",
-        error_near = "#3ba792",
-        error_far = "#5c9287",
         warn = "#e0af68",
-        warn_near = "#bf9e6e",
-        warn_far = "#9e8e76",
         info = "#0db9d7",
-        info_near = "#32a6ba",
-        info_far = "#58919c",
         hint = "#1abc9c",
-        hint_near = "#1abc9c",
-        hint_far = "#32a6ba",
         bg = bg('Normal'),
         cursor_bg = bg('CursorLine')
     },
